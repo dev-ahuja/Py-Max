992617b951487ba8bac689a6a7b6b8839d59e7f3
dev-ahuja <ahuja.devender@gmail.com>
2024-12-23 18:05:34 +0530

    new updates

---
+++ tute-dude/whilebreak.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''# while True:
+#     answer = input("enter some thing to print.\n Type 'done' to quit>")
+#     if answer == "done":
+#         break
+#     print(answer)
+
+# print("exiting")
+'''
+def subtract(arg1,arg2):
+    result = (arg1 - arg2)
+    return result
+
+subtract(20,9)
